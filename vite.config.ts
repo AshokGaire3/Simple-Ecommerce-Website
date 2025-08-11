c6b37debc5024810f34b58f2cb5bd1762da44df7
Ashok_G <117615959+AshIVg@users.noreply.github.com>
2025-08-10 23:52:29 -0400

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Simple-Ecommerce-Website/', // Set base for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
